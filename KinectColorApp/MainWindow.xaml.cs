a3519d81fec622b93497b2d07749c893540a9033
Evan Cann <ecann@umich.edu>
2014-10-05 19:07:05 -0700

    Added project

---
+++ KinectColorApp/MainWindow.xaml.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Kinect;
+
+namespace KinectColorApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private KinectSensor sensor;
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            if (KinectSensor.KinectSensors.Count > 0)
+            {
+                this.sensor = KinectSensor.KinectSensors[0];
+
+                if (this.sensor.Status == KinectStatus.Connected)
+                {
+                    this.sensor.ColorStream.Enable();
+                    this.sensor.DepthStream.Enable();
+                    this.sensor.SkeletonStream.Enable(); //-- Disabled for now, because we probably won't use skeletal tracking
+                    this.sensor.AllFramesReady += this.SensorAllFramesReady;
+                    //this.sensor.DepthFrameReady += sensor_DepthFrameReady;
+                    this.sensor.Start();
+                }
+            }
+        }
+
+        void sensor_DepthFrameReady(object sender, DepthImageFrameReadyEventArgs e)
+        {
+            //throw new NotImplementedException();
+            Console.WriteLine("depth frame ready");
+        }
+
+        private void SensorAllFramesReady(object sender, AllFramesReadyEventArgs e)
+        {
+            Console.WriteLine("here");
+            using (DepthImageFrame depthFrame = e.OpenDepthImageFrame())
+            {
+                if (depthFrame == null)
+                {
+                    Console.WriteLine("No depth frame");
+                    return;
+                }
+              
+                byte[] pixels = this.GenerateColoredBytes(depthFrame);
+
+                int stride = depthFrame.Width * 4;
+                image1.Source = BitmapSource.Create(depthFrame.Width, depthFrame.Height, 96, 96, PixelFormats.Bgr32, null, pixels, stride);
+            }
+        }
+
+        private byte[] GenerateColoredBytes(DepthImageFrame depthFrame)
+        {
+            short[] rawDepthData = new short[depthFrame.PixelDataLength];
+            depthFrame.CopyPixelDataTo(rawDepthData);
+
+            // Create the RGB pixel array
+            Byte[] pixels = new byte[depthFrame.Height * depthFrame.Width * 4];
+
+            const int BlueIndex = 0;
+            const int GreenIndex = 1;
+            const int RedIndex = 2;
+
+            // Loop through data and set colors for each pixel
+            for (int depthIndex = 0, colorIndex = 0;
+                 depthIndex < rawDepthData.Length && colorIndex < pixels.Length;
+                 depthIndex++, colorIndex += 4)
+            {
+                int player = rawDepthData[depthIndex] & DepthImageFrame.PlayerIndexBitmask;
+                int depth = rawDepthData[depthIndex] >> DepthImageFrame.PlayerIndexBitmaskWidth;
+                //Console.WriteLine(depth);
+                if (depth <= 900)
+                {
+                    pixels[colorIndex + BlueIndex] = 255;
+                    pixels[colorIndex + GreenIndex] = 0;
+                    pixels[colorIndex + RedIndex] = 0;
+                }
+                else if (depth > 900 && depth < 2000)
+                {
+                    pixels[colorIndex + BlueIndex] = 0;
+                    pixels[colorIndex + GreenIndex] = 255;
+                    pixels[colorIndex + RedIndex] = 0;
+                }
+                else if (depth >= 2000)
+                {
+                    pixels[colorIndex + BlueIndex] = 0;
+                    pixels[colorIndex + GreenIndex] = 0;
+                    pixels[colorIndex + RedIndex] = 255;
+                }
+
+                byte intensity = CalculateIntensityFromDepth(depth);
+                pixels[colorIndex + BlueIndex] = intensity;
+                pixels[colorIndex + GreenIndex] = intensity;
+                pixels[colorIndex + RedIndex] = intensity;
+            }
+
+            return pixels;
+        }
+
+        public static byte CalculateIntensityFromDepth(int distance)
+        {
+            return (byte)(255 - (255 * Math.Max(distance - 800, 0) / 2000));
+        }
+
+        void StopKinect(KinectSensor sensor)
+        {
+            if (sensor != null)
+            {
+                sensor.Stop();
+                sensor.AudioSource.Stop();
+            }
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            StopKinect(this.sensor);
+        }
+    }
+}
